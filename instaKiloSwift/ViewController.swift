db70b9cd88851ece980aa21319f4b90c86292862
Penjat <theSpencerSymington@Gmail.com>
2019-01-28 11:22:30 -0800

    Initial Commit

---
+++ instaKiloSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instaKiloSwift
+//
+//  Created by Spencer Symington on 2019-01-28.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
